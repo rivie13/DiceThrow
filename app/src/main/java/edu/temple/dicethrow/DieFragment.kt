c569e724b66f341f9538a45b50a011f6f82abecf
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-03-18 12:16:12 -0400

    fixed the bug where fragments stack on top of each other when rotating screen... check for saved state...

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -1,27 +1,32 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import kotlin.random.Random
 
 class DieFragment : Fragment() {
 
-    val DIESIDE = "sidenumber"
+    companion object {
+        private const val DIE_SIDES_KEY = "sidenumber"
+        private const val DIE_VALUE_KEY = "die_value"
+    }
 
     lateinit var dieTextView: TextView
-
     var dieSides: Int = 6
+    private var dieValue: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            it.getInt(DIESIDE).run {
-                dieSides = this
-            }
+            dieSides = it.getInt(DIE_SIDES_KEY, 6) // Default to 6 if not provided
+        }
+        // Restore the dieValue if it exists in the savedInstanceState
+        if (savedInstanceState != null) {
+            dieValue = savedInstanceState.getInt(DIE_VALUE_KEY, 0)
         }
     }
 
@@ -37,13 +42,27 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
-        view.setOnClickListener{
+
+        // If it's the first time, throw the die. Otherwise, display the saved value.
+        if (savedInstanceState == null) {
+            throwDie()
+        } else {
+            dieTextView.text = dieValue.toString()
+        }
+
+        view.setOnClickListener {
             throwDie()
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        // Save the current dieValue to the bundle
+        outState.putInt(DIE_VALUE_KEY, dieValue)
+    }
+
     fun throwDie() {
-        dieTextView.text = (Random.nextInt(dieSides) + 1).toString()
+        dieValue = Random.nextInt(dieSides) + 1
+        dieTextView.text = dieValue.toString()
     }
 }
